f6dcef69f019a3409ee6626e16cd0592cbdcb301
Thecodingnerd88 <40854335+thecodingnerd88@users.noreply.github.com>
2018-09-26 16:18:09 +0300

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,17 @@
+var lis = document.querySelectorAll("li");
+
+for (var i=0 ; i < lis.length ; i++){
+    lis[i].addEventListener("click", function (){
+        this.classList.toggle ("done");
+    });
+};
+for (var i=0 ; i < lis.length ; i++){
+    lis[i].addEventListener("mouseover", function (){
+        this.classList.add("onItem");
+    });
+};
+for (var i=0 ; i < lis.length ; i++){
+    lis[i].addEventListener("mouseout", function(){
+        this.classList.remove("onItem");
+    });
+};
